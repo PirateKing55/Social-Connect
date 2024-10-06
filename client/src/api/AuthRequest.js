301676dfd50211bb5973e871a2d4a531451bed65
snehal <agrawalsnehal327@gmail.com>
2024-10-07 01:43:48 +0530

    updated servel url

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:4000' });
+const API = axios.create({ baseURL: 'https://social-connect-r4my.onrender.com' });
 
-export const logIn = (formData) => API.post('/auth/login', formData); 
+export const logIn = (formData) => API.post('/auth/login', formData);
 
 export const signUp = (formData) => API.post('/auth/register', formData);
